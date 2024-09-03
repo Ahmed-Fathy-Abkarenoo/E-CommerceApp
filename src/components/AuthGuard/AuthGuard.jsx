fa53167d59818822ccbab21e6acd44984b1aaed6
Ahmed-Fathy-Abkarenoo <a_abomosalam@hotmail.com>
2024-09-03 15:54:24 +0300

    path

--- src/components/AuthGuard/AuthGuard.jsx
+++ src/components/AuthGuard/AuthGuard.jsx
@@ -1,14 +1,9 @@
 import React, { useContext } from "react";
-import { useState, useEffect } from "react";
 import { AuthContext } from "../../Contexts/AuthContext";
 import { Navigate } from "react-router-dom";
 
 export default function AuthGuard({ children }) {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {}, []);
-
   const { userToken } = useContext(AuthContext);
 
-  return <>{!userToken ? children : <Navigate to={"/home"} />}</>;
+  return <>{!userToken ? children : <Navigate to={"home"} />}</>;
 }
